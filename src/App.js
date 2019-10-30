20fcae1dcf6ac9d588dc41f818da2bdf22ceda91
Ravee Kirann <raveekirann@Ravee-Kiranns-Mac-2.local>
2019-10-30 21:46:19 +0530

    added travis deploy aws config

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi Nikhilesh
-          HI Mohit
+          Hi Mohit
         </p>
         <a
           className="App-link"
